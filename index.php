ad927de29dd955ea8644cad25a26c9dea3cf54a7
Raul Villareal <rvillarrcal@cnm.edu>
2016-07-12 15:27:53 -0600

    added css and js folders and linked them to the page

--- index.php
+++ index.php
@@ -1,24 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+		<!-- unicode for international languages-->
 		<meta charset="utf-8"/>
+		<!-- adding css to the page-->
+		<link href="css/style.css" rel="stylesheet" type="text/css"/>
+		<!-- add javascript content-->
+		<script src="js/custom.js" type="text/javascript"></script>
+		<!-- title should be meaningful -->
 		<title>Basic HTML Page</title>
 	</head>
 	<body>
-		<h1>"Hello World!!"</h1>
-		<p>Lorem Khaled Ipsum is a major key to success. To be successful you’ve got to work hard, to make history, simple, you’ve got to make it. Always remember in the jungle there’s a lot of they in there, after you overcome they, you will make it to paradise. Bless up. Celebrate success right, the only way, apple. A major key, never panic. Don’t panic, when it gets crazy and rough, don’t panic, stay calm. In life you have to take the trash out, if you have trash in your life, take it out, throw it away, get rid of it, major key.</p>
+		<!-- functions like header, footer, main, are used for semantics purposes -->
+		<header>
+			<h1>"Hello World!!"</h1>
+		</header>
+		<!-- Start main content here -->
+		<main>
+		<p>Lorem Khaled Ipsum is a major key to <strong>success</strong>. To be successful you’ve got to <em> work</em> hard, to make history, simple, you’ve got to make it. Always remember in the jungle there’s a lot of they in there, after you overcome they, you will make it to paradise. Bless up. Celebrate success right, the only way, apple. A major key, never panic. Don’t panic, when it gets crazy and rough, don’t panic, stay calm. In life you have to take the trash out, if you have trash in your life, take it out, throw it away, get rid of it, major key.</p>
 		<h2>About me</h2>
 		<p>The weather is amazing, walk with me through the pathway of more success. Take this journey with me, Lion! The key to success is to keep your head above the water, never give up. Cloth talk. You see that bamboo behind me though,</p>
+		<!-- unordered list doesn't give a value or organized tags to comments -->
 		<ul>
 			<li>primer cosa</li>
 			<li>Segunda cosa</li>
 			<li>Tercer grado</li>
 		</ul>
-		<ol>
+		<!-- ordered lists give a number 1,2,3 -->
+			<ol>
 			<li>Strike one</li>
 			<li>Strike two</li>
 			<li>Strike three</li>
 		</ol>
-		<a href="https://www.khanacademy.org"  target="_blank"> Go to khanacademy.org </a>
+		<div>
+			<a href="https://www.khanacademy.org"  target="_blank"> Go to khanacademy.org </a>
+			<img src="images/khanlogo.jpg" alt="Khan Academy" />
+		</div>
+		<table>
+			<tr>
+				<th>first names</th>
+				<th>last name</th>
+			</tr>
+			<tr>
+				<td>table cell3</td>
+				<td>table cell 4</td>
+			</tr>
+		</table>
+		<footer>Ya me salio la wezait!!</footer>
+		</main>
 	</body>
 </html>

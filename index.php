de1898290b518b2a517fef4a0da8374e645ff137
Raul Villareal <rvillarrcal@cnm.edu>
2016-07-12 12:51:57 -0600

    added paragraph lists, and link

--- index.php
+++ index.php
@@ -6,5 +6,19 @@
 	</head>
 	<body>
 		<h1>"Hello World!!"</h1>
+		<p>Lorem Khaled Ipsum is a major key to success. To be successful you’ve got to work hard, to make history, simple, you’ve got to make it. Always remember in the jungle there’s a lot of they in there, after you overcome they, you will make it to paradise. Bless up. Celebrate success right, the only way, apple. A major key, never panic. Don’t panic, when it gets crazy and rough, don’t panic, stay calm. In life you have to take the trash out, if you have trash in your life, take it out, throw it away, get rid of it, major key.</p>
+		<h2>About me</h2>
+		<p>The weather is amazing, walk with me through the pathway of more success. Take this journey with me, Lion! The key to success is to keep your head above the water, never give up. Cloth talk. You see that bamboo behind me though,</p>
+		<ul>
+			<li>primer cosa</li>
+			<li>Segunda cosa</li>
+			<li>Tercer grado</li>
+		</ul>
+		<ol>
+			<li>Strike one</li>
+			<li>Strike two</li>
+			<li>Strike three</li>
+		</ol>
+		<a href="https://www.khanacademy.org"  target="_blank"> Go to khanacademy.org </a>
 	</body>
 </html>
